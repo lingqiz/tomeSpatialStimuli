253e24197bcdfaed2d44b690e45d5a4497f3b28d
bock42 <abock@mac-pro.uphs.upenn.edu>
2016-09-02 15:36:49 -0400

    play_pRF - now saves 'imagesFull' in params

--- pRF/play_pRF.m
+++ pRF/play_pRF.m
@@ -22,79 +22,79 @@ function play_pRF(saveInfo,imagesFull,TR,scanDur,display,tChar,rChar)
 
 %% Set defaults
 % Get git repository information
-fCheck = which('GetGitInfo');
+fCheck                          = which('GetGitInfo');
 if ~isempty(fCheck)
-    thePath = fileparts(mfilename('fullpath'));
-    gitInfo = GetGITInfo(thePath);
+    thePath                     = fileparts(mfilename('fullpath'));
+    gitInfo                     = GetGITInfo(thePath);
 else
-    gitInfo = 'function ''GetGITInfo'' not found';
+    gitInfo                     = 'function ''GetGITInfo'' not found';
 end
 % Get user name
-[~, tmpName] = system('whoami');
-userName = strtrim(tmpName);
+[~, tmpName]                    = system('whoami');
+userName                        = strtrim(tmpName);
 % Set Dropbox directory
 dbDir = ['/Users/' userName '/Dropbox (Aguirre-Brainard Lab)'];
 disp(['Dropbox directory = ' dbDir]);
 % Load images
 if ~exist('imagesFull','var') || isempty(imagesFull)
-    imFile = fullfile(dbDir,'TOME_materials','StimulusFiles','pRFimages.mat');
+    imFile                      = fullfile(dbDir,'TOME_materials','StimulusFiles','pRFimages.mat');
     disp('Loading pRF images file...');
-    tmp = load(imFile);
-    imagesFull = tmp.imagesFull;
+    tmp                         = load(imFile);
+    imagesFull                  = tmp.imagesFull;
     clear tmp;
 end
 % TR
 if ~exist('TR','var') || isempty(TR)
-    TR = 0.8;
+    TR                          = 0.8;
 end
 % scan duration
 if ~exist('scanDur','var') || isempty(scanDur)
-    scanDur = 336; % seconds
+    scanDur                     = 336; % seconds
 end
 % dispaly parameters
 if ~exist('display','var') || isempty(display)
-    display.distance = 106.5; % distance from screen (cm) - (UPenn - SC3T);
-    display.width = 69.7347; % width of screen (cm) - (UPenn - SC3T);
-    display.height = 39.2257; % height of screen (cm) - (UPenn - SC3T);
+    display.distance            = 106.5; % distance from screen (cm) - (UPenn - SC3T);
+    display.width               = 69.7347; % width of screen (cm) - (UPenn - SC3T);
+    display.height              = 39.2257; % height of screen (cm) - (UPenn - SC3T);
 end
 % scanner trigger
 if ~exist('tChar','var') || isempty(tChar)
-    tChar = {'t'};
+    tChar                       = {'t'};
 end
 % scanner trigger
 if ~exist('rChar','var') || isempty(rChar)
-    rChar = {'r' 'g' 'b' 'y'};
+    rChar                       = {'r' 'g' 'b' 'y'};
 end
 % Save input variables
-params.functionName     = mfilename;
-params.gitInfo          = gitInfo;
-params.userName         = userName;
-params.subjectName      = saveInfo.subjectName;
-params.TR               = TR;
-params.scanDur          = scanDur;
+params.functionName             = mfilename;
+params.gitInfo                  = gitInfo;
+params.userName                 = userName;
+params.subjectName              = saveInfo.subjectName;
+params.TR                       = TR;
+params.scanDur                  = scanDur;
 %% For Trigger
-a = cd;
+a                               = cd;
 if a(1)=='/' % mac or linux
-    a = PsychHID('Devices');
+    a                           = PsychHID('Devices');
     for i = 1:length(a)
-        d(i) = strcmp(a(i).usageName, 'Keyboard');
+        d(i)                    = strcmp(a(i).usageName, 'Keyboard');
     end
-    keybs = find(d);
+    keybs                       = find(d);
 else % windows
-    keybs = [];
+    keybs                       = [];
 end
 %% Make fixation dot color changes
 if ~exist('redFrames','var') || isempty(redFrames)
-    maxFrames = size(imagesFull,3);
-    redFrames = zeros(1,maxFrames);
-    minDiff = 0;
-    maxDiff = inf;
-    minTime = 5; % dot color changes are separated by at least 5s
-    maxTime = 30; % dot color changes are separated no more than 30s
+    maxFrames                   = size(imagesFull,3);
+    redFrames                   = zeros(1,maxFrames);
+    minDiff                     = 0;
+    maxDiff                     = inf;
+    minTime                     = 5; % dot color changes are separated by at least 5s
+    maxTime                     = 30; % dot color changes are separated no more than 30s
     while minDiff < (minTime * 8) || maxDiff > (maxTime * 8) % (8 frames / sec)
-        switches = sort(randperm(maxFrames,ceil(maxFrames / (20 * 8) ))); % ~every 20s
-        minDiff = min(diff(switches));
-        maxDiff = max(diff(switches));
+        switches                = sort(randperm(maxFrames,ceil(maxFrames / (20 * 8) ))); % ~every 20s
+        minDiff                 = min(diff(switches));
+        maxDiff                 = max(diff(switches));
     end
     ct = 0;
     % Make a vector of 0's (green) and 1's (red), to use for chaging the
@@ -103,38 +103,38 @@ if ~exist('redFrames','var') || isempty(redFrames)
         if ct
             if i ~= length(switches)
                 redFrames(switches(i):switches(i+1)) = 1;
-                ct = 0;
+                ct              = 0;
             else
                 redFrames(switches(i):end) = 1;
-                ct = 0;
+                ct              = 0;
             end
         else
-            ct = ct + 1;
+            ct                  = ct + 1;
         end
     end
 end
 %% Initial settings
 PsychDefaultSetup(2);
 Screen('Preference', 'SkipSyncTests', 2); % Skip sync tests
-screens = Screen('Screens'); % get the number of screens
-screenid = max(screens); % draw to the external screen
+screens                         = Screen('Screens'); % get the number of screens
+screenid                        = max(screens); % draw to the external screen
 %% Define black and white
-white = WhiteIndex(screenid);
-black = BlackIndex(screenid);
-grey = white/2;
+white                           = WhiteIndex(screenid);
+black                           = BlackIndex(screenid);
+grey                            = white/2;
 %% Screen params
-res = Screen('Resolution',max(Screen('screens')));
-display.resolution = [res.width res.height];
+res                             = Screen('Resolution',max(Screen('screens')));
+display.resolution              = [res.width res.height];
 PsychImaging('PrepareConfiguration');
 PsychImaging('AddTask', 'General', 'UseRetinaResolution');
-[winPtr, windowRect] = PsychImaging('OpenWindow', screenid, grey);
-[mint,~,~] = Screen('GetFlipInterval',winPtr,100);
-display.frameRate = 1/mint; % 1/monitor flip interval = framerate (Hz)
-display.screenAngle = pix2angle( display, display.resolution );
-[screenXpix, screenYpix] = Screen('WindowSize', winPtr);% Get the size of the on screen window
-[center(1), center(2)] = RectCenter(windowRect); % Get the center coordinate of the window
-fix_mask = angle2pix(display,0.75); % For fixation mask (0.75 degree)
-fix_dot = angle2pix(display,0.25); % For fixation cross (0.25 degree)
+[winPtr, windowRect]            = PsychImaging('OpenWindow', screenid, grey);
+[mint,~,~]                      = Screen('GetFlipInterval',winPtr,100);
+display.frameRate               = 1/mint; % 1/monitor flip interval = framerate (Hz)
+display.screenAngle             = pix2angle( display, display.resolution );
+[screenXpix, screenYpix]        = Screen('WindowSize', winPtr);% Get the size of the on screen window
+[center(1), center(2)]          = RectCenter(windowRect); % Get the center coordinate of the window
+fix_mask                        = angle2pix(display,0.75); % For fixation mask (0.75 degree)
+fix_dot                         = angle2pix(display,0.25); % For fixation cross (0.25 degree)
 %% Dot stimulus params
 % Set the blend function so that we get nice antialised edges
 Screen('BlendFunction', winPtr, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -160,15 +160,15 @@ try
     disp('Ready, waiting for trigger...');
     startTime = wait4T(tChar);  %wait for 't' from scanner.
     %% Drawing Loop
-    breakIt = 0;
-    Keyct = 0;
-    Rct = 0;
-    Gct = 0;
-    curFrame = 1;
-    params.startDateTime    = datestr(now);
-    params.endDateTime      = datestr(now); % this is updated below
-    params.GreenDotTime        = [];
-    params.RedDotTime          = [];
+    breakIt                     = 0;
+    Keyct                       = 0;
+    Rct                         = 0;
+    Gct                         = 0;
+    curFrame                    = 1;
+    params.startDateTime        = datestr(now);
+    params.endDateTime          = datestr(now); % this is updated below
+    params.GreenDotTime         = [];
+    params.RedDotTime           = [];
     disp(['Trigger received - ' params.startDateTime]);
     while GetSecs-startTime < scanDur && ~breakIt  %loop until 'esc' pressed or time runs out
         % update timers
@@ -205,7 +205,7 @@ try
         end
         % Flip to the screen
         Screen('Flip', winPtr);
-        params.endDateTime = datestr(now);
+        params.endDateTime      = datestr(now);
         WaitSecs(0.001);
     end
     sca;
@@ -214,7 +214,8 @@ try
     ShowCursor;
     Screen('CloseAll');
     %% Save params
-    params.display = display;
+    params.display              = display;
+    params.imagesFull           = imagesFull;    
     save(saveInfo.fileName,'params');
 catch ME
     Screen('CloseAll');
