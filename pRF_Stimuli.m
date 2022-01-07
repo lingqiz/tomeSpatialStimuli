037dcd095b450764e8b085a18ce0a1e04e468413
lingqiz <zlqzcc@gmail.com>
2022-01-07 14:42:21 -0500

    minor update to start script

--- pRF_Stimuli.m
+++ pRF_Stimuli.m
@@ -1,16 +1,20 @@
 %% Play the retinotopic stimulus
 
-% Add path
+%% Add path
 addpath(fullfile('.', 'pRF'));
 
-% Define inputs
+%% Generate image stimuli
 imFile = './data/imFile.mat'; % Output file that saves the images
 imagesFull = make_bars(imFile); % Create the images that will be displayed
 
-% Save info
-savePath = './data/subData.mat';
-saveInfo.subjectName = 'LQZ';
+%% Save info
+sID = 1; subName = 'LQZ';
+fName = strcat(subName, '_', num2str(sID), '.mat');
+savePath = fullfile('.', 'data', fName);
+
+saveInfo.subjectName = subName;
 saveInfo.fileName = savePath;
 
-% Play the stimulus
-play_pRF(saveInfo, imagesFull);
+%% Play the stimulus
+stimFile = load(fullfile('.', 'data', 'imFile.mat'));
+play_pRF(saveInfo, stimFile.imagesFull);
